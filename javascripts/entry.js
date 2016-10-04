cfab6cda0ac30c9012c056c86063e0942278170e
Scott Duane <adrian.scott.duane@gmail.com>
2016-10-03 20:20:40 -0700

    Add gradient buttons

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -1,9 +1,8 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   let ctx = document.getElementById('canvas-el');
-  let speed = 1000;
+  let speed = 2000;
   let automata = new Automata('hexagon', speed);
   let board = new Board('hexagon', ctx, automata);
   board.startGame();
 });
-
